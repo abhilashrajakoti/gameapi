e857292a42bc17abf78ce3ddcf8e8ebe05e09a86
abhilashrajakoti <abhi.delfiten@gmail.com>
2017-10-15 16:34:37 +0530

    removed unncessary comments

--- src/main/java/com/api/game/gameapi/GameRestAPI.java
+++ src/main/java/com/api/game/gameapi/GameRestAPI.java
@@ -11,33 +11,24 @@ import org.apache.log4j.Logger;
 import com.core.game.CricketFieldPixel;
 import com.core.game.GameEngine;
 
-/**
- * Root resource (exposed at "myresource" path)
- */
 @Path("/pixels")
 public class GameRestAPI {
 
 	final static Logger logger = Logger.getLogger(GameRestAPI.class);
 
-	/**
-     * Method handling HTTP GET requests. The returned object will be sent
-     * to the client as "text/plain" media type.
-     *
-     * @return String that will be returned as a text/plain response.
-     */
-    @GET
-    @Path("/{pixelId}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public CricketFieldPixel getIt(@PathParam("pixelId") int pixelId) {
-    	CricketFieldPixel pixel=null;
-    	try {
-        GameEngine game=new GameEngine();
-        pixel=game.getFieldInfo(pixelId);
-        logger.info(pixel.getId()+"::"+pixel.getName());;
-    	} 
-    	catch(Exception e) {
-    		logger.error("GameAPIError", e);
-    	}
-    	return pixel;
-    }
+	@GET
+	@Path("/{pixelId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public CricketFieldPixel getIt(@PathParam("pixelId") int pixelId) {
+		CricketFieldPixel pixel = null;
+		try {
+			GameEngine game = new GameEngine();
+			pixel = game.getFieldInfo(pixelId);
+			logger.info(pixel.getId() + "::" + pixel.getName());
+			;
+		} catch (Exception e) {
+			logger.error("GameAPIError", e);
+		}
+		return pixel;
+	}
 }
