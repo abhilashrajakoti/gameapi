b2db06d47f6efc4ad691a60e11ef44f8c0f27e9f
abhilashrajakoti <abhi.delfiten@gmail.com>
2017-10-20 16:55:43 +0530

    get all pixels json

--- src/main/java/com/api/game/gameapi/GameRestAPI.java
+++ src/main/java/com/api/game/gameapi/GameRestAPI.java
@@ -1,5 +1,7 @@
 package com.api.game.gameapi;
 
+import java.util.List;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -19,16 +21,29 @@ public class GameRestAPI {
 	@GET
 	@Path("/{pixelId}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public CricketFieldPixel getIt(@PathParam("pixelId") int pixelId) {
+	public CricketFieldPixel getPositionName(@PathParam("pixelId") int pixelId) {
 		CricketFieldPixel pixel = null;
 		try {
 			GameEngine game = new GameEngine();
-			pixel = game.getFieldInfo(pixelId);
+			pixel = game.getFieldPostionsInfo(pixelId);
 			logger.info(pixel.getId() + "::" + pixel.getName());
-			;
 		} catch (Exception e) {
 			logger.error("GameAPIError", e);
 		}
 		return pixel;
+	}
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<CricketFieldPixel> getAllPositions() {
+		List<CricketFieldPixel> list = null;
+		try {
+			GameEngine game = new GameEngine();
+			list = game.getAllFieldPostions();
+		} catch (Exception e) {
+			logger.error("GameAPIError", e);
+		}
+		return list;
+
 	}
 }
